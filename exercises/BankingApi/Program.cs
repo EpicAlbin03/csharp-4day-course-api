d40ec26206766586d7a014ec2ca504ec3bdf71c8
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-04-28 11:49:56 +0200

    enum names

--- exercises/BankingApi/Program.cs
+++ exercises/BankingApi/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using BankingApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
@@ -6,7 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
